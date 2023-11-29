b9470af9e817e6d9e916aecd04a36c170dd4f1fe
Gabriel Fernando Silva de Souza <gabriel@Gabriels-MacBook-Air.local>
2023-11-28 22:32:41 -0300

    first

---
+++ proxy.tf
@@ -0,0 +1,53 @@
+data "aws_caller_identity" "current" {}
+
+# RDS PROXY CONFIGS
+
+resource "aws_db_proxy" "rdsproxy" {
+
+  name                   = var.proxy_name
+  debug_logging          = false
+  engine_family          = var.engine_family
+  idle_client_timeout    = 180
+  require_tls            = true
+  role_arn               = "arn:aws:iam::975635808270:role/AdminAccess"
+  vpc_security_group_ids = var.vpc_security_group_ids
+  vpc_subnet_ids         = var.vpc_subnet_ids
+
+  auth {
+    auth_scheme = "SECRETS"
+    description = "Conection with Secret Manager"
+    iam_auth = "DISABLED"
+    secret_arn = var.secret_arn
+  }
+}
+
+resource "aws_db_proxy_default_target_group" "rdsproxy_tg" {
+  
+  db_proxy_name = aws_db_proxy.rdsproxy.name
+  
+  connection_pool_config {
+    connection_borrow_timeout = 180
+    max_connections_percent   = 100
+    max_idle_connections_percent = 20
+  }
+}
+
+resource "aws_db_proxy_target" "rdsproxy_target" {
+  db_proxy_name = aws_db_proxy.rdsproxy.name
+  target_group_name = aws_db_proxy_default_target_group.rdsproxy_tg.name
+  db_instance_identifier = var.db_instance_identifier
+}
+
+resource "aws_db_proxy_endpoint" "rds_endpoint" {
+
+  db_proxy_name = aws_db_proxy.rdsproxy.name
+
+  db_proxy_endpoint_name = var.endpoint_name
+
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  vpc_subnet_ids = var.vpc_subnet_ids
+
+  target_role = "READ_WRITE"
+
+}
---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable proxy_name {
+  type        = string
+  default     = "proxy-test"
+  description = "description"
+}
+
+variable engine_family {
+  type        = string
+  default     = "MYSQL"
+  description = "description"
+}
+
+variable vpc_security_group_ids {
+  type        = list
+  default     = ["sg-0d78689315ff00ae3"]
+  description = "description"
+}
+
+variable vpc_subnet_ids {
+  type        = list
+  default     = ["subnet-0807fd8c6bdfb7d2b", "subnet-086118b07bc20a940"]
+  description = "description"
+}
+
+variable secret_arn {
+  type        = string
+  default     = "arn:aws:secretsmanager:us-east-2:975635808270:secret:proxy-qFa1kd"
+  description = "description"
+}
+
+variable db_instance_identifier {
+  type        = string
+  default     = "test"
+  description = "description"
+}
+
+variable endpoint_name {
+  type        = string
+  default     = "proxy-test"
+  description = "description"
+}
+
+
+
+
+
+
+
+
